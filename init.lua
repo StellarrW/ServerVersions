e5139fe4b336dbc10042af5901b44f0bb2cf59eb
Stefan <42220813+Stefanuk12@users.noreply.github.com>
2024-05-08 18:21:33 +0100

    dh update

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 return {
     --[[ [Da Hood](https://www.roblox.com/games/2788229376) ]]--
-    [2788229376] = "(5cc74c)", -- Regular
-    [16033173781] = "(5cc74c)", -- Macro
+    [2788229376] = "(9e3358)", -- Regular
+    [16033173781] = "(9e3358)", -- Macro
 }
